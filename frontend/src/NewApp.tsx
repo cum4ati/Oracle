610da61f509d7963f8b16b1977113a45188ef028
Dmitriy Trofimov <princessselestiapwnz@mail.ru>
2022-02-10 23:35:55 +0400

    Refactored some code

--- frontend/src/NewApp.tsx
+++ frontend/src/NewApp.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import Shakespeare from "./pages/Shakespeare";
 
 class MainApp extends React.Component<{}, { pageToDisplay: string }> {
