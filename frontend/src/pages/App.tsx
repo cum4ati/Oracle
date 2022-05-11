34ee5e2d0721ad3a24d5865d1ab22d78a664688f
Dmitriy Trofimov <princessselestiapwnz@mail.ru>
2022-05-11 16:14:46 +0400

    страница авторизации

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../App.css';
 import MainApp from "./NewApp";
+import Register from "./Register"
 
 function App() {
-  const app = new MainApp({});
-  return (
-   app.render()
-  );
+  // const app = new MainApp({});
+  // return (
+  //  app.render()
+  // );
+  return Register()
 }
 
 export default App;
